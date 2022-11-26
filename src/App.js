011a7de9633d2090407564d393277d54d295657d
Pradeep Kaswan <76137397+kaswanp1406@users.noreply.github.com>
2022-11-26 17:40:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist//css/bootstrap.min.css";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
+import React from "react";
+
+const CLIENT_ID = "52521781f9d64d6a917cb8bc95932e5f";
+const CLIENT_SECRET = "9d270766045b4586a3f514776aff1e15";
 
 function App() {
+  const [searchInput, setsearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    //API Access Token
+    var authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => setAccessToken(data.access_token));
+  }, []);
+
+  // Search
+  async function search() {
+    console.log("Search for " + searchInput);
+
+    var searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': "Bearer " + accessToken,
+      },
+    };
+
+    var artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => { return data.artists.items[0].id });
+
+      var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', searchParameters)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setAlbums(data.items);
+      })
+  }
+  console.log(albums);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="mt-3">
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Search for Artist"
+            type="input"
+            onKeyPress={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setsearchInput(event.target.value)}
+          />
+          <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map( (album, i) => {
+            return (
+              <Card>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            )
+          })}
+          
+        </Row>
+      </Container>
     </div>
   );
 }
